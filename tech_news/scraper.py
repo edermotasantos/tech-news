77c1bf3cd1a4e582af3c20ca949039577f37405e
Eder Santos <3derms@gmail.com>
2022-03-11 10:21:11 -0300

    feat: Cria as funções scrape_next_page_link e scrape_noticia

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import parsel
 
 
 def fetch(url):
@@ -17,12 +18,25 @@ def fetch(url):
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = parsel.Selector(html_content)
+    css_classes_prefix = ".tec--list.tec--list--lg"
+    css_classes_suffix = " a.tec--card__thumb__link::attr(href)"
+    url_list = selector.css(
+        css_classes_prefix + css_classes_suffix
+        ).getall()
+    return url_list
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = parsel.Selector(html_content)
+    div = ".tec--list.tec--list--lg"
+    url_next_class_prefix = " a.tec--btn.tec--btn--lg.tec--btn--primary"
+    css_class_suffix = ".z--mx-auto.z--mt-48::attr(href)"
+    next_page = selector.css(
+        div + url_next_class_prefix + css_class_suffix
+        ).get()
+    return next_page
 
 
 # Requisito 4
