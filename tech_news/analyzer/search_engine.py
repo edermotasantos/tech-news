3b2d57390ab86cbf3d0f601482aafed072dd4ce2
Eder Santos <3derms@gmail.com>
2022-03-14 22:04:43 -0300

    feat: Cria a função search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 import re
+import datetime
 from tech_news.database import (
     search_news,
 )
@@ -7,19 +8,30 @@ from tech_news.database import (
 # Requisito 6
 def search_by_title(title):
     list = []
-
     # re.compile(title, re.IGNORECASE) para "case insensitive"
     titles_news = search_news({"title": re.compile(title, re.IGNORECASE)})
-
     for title_news in titles_news:
         list.append((title_news["title"], title_news["url"]))
-
     return list
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    date_format = "%Y-%m-%d"
+    list = []
+    try:
+        is_valid_date = datetime.datetime.strptime(date, date_format)
+        if is_valid_date:
+            dates_news = search_news(
+                {
+                    "timestamp": re.compile(date, re.IGNORECASE)
+                }
+            )
+            for date_news in dates_news:
+                list.append((date_news["title"], date_news["url"]))
+            return list
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
